66ae0482e321e5a3d6d1d91b3d6019a4d71e3acf
ADILSE2029 <adilamiinn@gmail.com>
2026-02-21 20:16:23 +0500

    Complete Version

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
 import module1 as m1
-bunkBuddy = m1.Bunkbuddy()
-print("\t\t\t===== ATTENDENCE TRACKER =====")
-while True:
-    m1.custom_decorator(117)
-    user_given_choice_at_start_about_process = input(
-        "[M] Mark Attendence\n[V] View Details\n[R] Reset Data\n[W] Warnings\n[B] Bunk-o-Meter\n[Q] Quit\nChoice:")
-    if user_given_choice_at_start_about_process == "M" or user_given_choice_at_start_about_process == "m":
-        m1.decorator()
-        bunkBuddy.subject_selection()
-    elif user_given_choice_at_start_about_process == "V" or user_given_choice_at_start_about_process == "v":
-        m1.decorator()
 
-        bunkBuddy.view_details()
-    elif user_given_choice_at_start_about_process == "R" or user_given_choice_at_start_about_process == "r":
-        bunkBuddy.reset_data()
-    elif user_given_choice_at_start_about_process == "Q" or user_given_choice_at_start_about_process == "q":
-        m1.decorator()
-        print("Thanks for using ")
-        break
-    elif user_given_choice_at_start_about_process == "W" or user_given_choice_at_start_about_process == "w":
-        m1.decorator()
-        bunkBuddy.warnings()
-    elif user_given_choice_at_start_about_process == "B" or user_given_choice_at_start_about_process == "b":
-        m1.decorator()
-        bunkBuddy.bunkmeter()
-    else:
-        m1.decorator()
-        print("Invalid Input")
+
+def main():
+
+    print("\t\t\t===== ATTENDENCE TRACKER =====")
+    while True:
+        bunkBuddy = m1.Bunkbuddy()
+        m1.custom_decorator(117)
+        user_choice = input(
+            "[M] Mark Attendence\n[V] View Details\n[R] Reset Data\n[W] Warnings\n[B] Bunk-o-Meter\n[Q] Quit\nChoice:")
+        if user_choice == "M" or user_choice == "m":
+            m1.decorator()
+            bunkBuddy.subject_selection()
+        elif user_choice == "V" or user_choice == "v":
+            m1.decorator()
+
+            bunkBuddy.view_details()
+        elif user_choice == "R" or user_choice == "r":
+            bunkBuddy.reset_data()
+        elif user_choice == "Q" or user_choice == "q":
+            m1.decorator()
+            print("Thanks for using ")
+            break
+        elif user_choice == "W" or user_choice == "w":
+            m1.decorator()
+            bunkBuddy.warnings()
+        elif user_choice == "B" or user_choice == "b":
+            m1.decorator()
+            bunkBuddy.bunkmeter()
+        else:
+            m1.decorator()
+            print("Invalid Input")
+
+
+if __name__ == "__main__":
+    main()
--- module1.py
+++ module1.py
@@ -1,4 +1,10 @@
+import os
+
+
 class Bunkbuddy:
+    def __init__(self):
+        self.filename = "bunkbuddy"
+
     def subject_selection(self):
         try:
             which_subject_at_start = int(input(
@@ -15,14 +21,16 @@ class Bunkbuddy:
                 self.attendence_marker(4)
             elif which_subject_at_start == 6:
                 self.attendence_marker(5)
-            elif which_subject_at_start:
+            else:
                 print("Subject Not Found")
         except ValueError:
             print("Invalid Input,input must be a number")
 
     def attendence_marker(self, which_subject_to_mark):
-
-        with open("bunkbuddy.txt", "r") as f:
+        if not os.path.exists(self.filename):
+            print("Storage File Does not Exist,Use Reset Function at main menu")
+            return
+        with open(self.filename, "r") as f:
             subject_list = f.readlines()
             subject_data = subject_list[which_subject_to_mark].strip().split(
                 '|')
@@ -31,44 +39,36 @@ class Bunkbuddy:
             subject_my_attend = int(subject_data[2])
             subject_my_absent = int(subject_data[3])
             subject_canceled = int(subject_data[4])
-            try:
-                given_attendence = input(
-                    "[P]resent or [A]bsent or [C]anceled?\nMark:")
-                if given_attendence == "P" or given_attendence == "p":
-                    subject_total_attend += 1
-                    subject_my_attend += 1
-                    percentage = subject_my_attend/subject_total_attend*100
-                    new_line = f"{subject_name}|{subject_total_attend}|{subject_my_attend}|{subject_my_absent}|{subject_canceled}|{percentage}\n"
-                    subject_list[which_subject_to_mark] = new_line
-                    with open("bunkbuddy.txt", "w") as f:
-                        for each_subject in subject_list:
-                            f.write(each_subject)
-                        print("\t--Attendence Marked--")
-                elif given_attendence == "A" or given_attendence == "a":
-                    subject_total_attend += 1
-                    subject_my_absent += 1
-                    percentage = subject_my_attend/subject_total_attend*100
-                    new_line = f"{subject_name}|{subject_total_attend}|{subject_my_attend}|{subject_my_absent}|{subject_canceled}|{percentage}\n"
-                    subject_list[which_subject_to_mark] = new_line
-                    with open("bunkbuddy.txt", "w") as f:
-                        for each_subject in subject_list:
-                            f.write(each_subject)
-                        print("\t--Attendence Marked--")
-                elif given_attendence == "C" or given_attendence == "c":
-                    subject_canceled += 1
-                    percentage = subject_my_attend/subject_total_attend*100
-                    new_line = f"{subject_name}|{subject_total_attend}|{subject_my_attend}|{subject_my_absent}|{subject_canceled}|{percentage}\n"
-                    subject_list[which_subject_to_mark] = new_line
-                    with open("bunkbuddy.txt", "w") as f:
-                        for each_subject in subject_list:
-                            f.write(each_subject)
-                        print("\t--Attendence Marked--")
-                else:
-                    print("Invalid Input")
-            except ZeroDivisionError:
-                print("No Class Data Available")
+            given_attendence = input(
+                "[P]resent or [A]bsent or [C]anceled?\nMark:").lower()
+            if given_attendence == "P" or given_attendence == "p":
+                subject_total_attend += 1
+                subject_my_attend += 1
+
+            elif given_attendence == "A" or given_attendence == "a":
+                subject_total_attend += 1
+                subject_my_absent += 1
+
+            elif given_attendence == "C" or given_attendence == "c":
+                subject_canceled += 1
+            else:
+                print("Invalid Input")
+                return
+            if subject_total_attend > 0:
+                percentage = subject_my_attend/subject_total_attend*100
+            else:
+                percentage = 0
+            new_line = f"{subject_name}|{subject_total_attend}|{subject_my_attend}|{subject_my_absent}|{subject_canceled}|{percentage}\n"
+            subject_list[which_subject_to_mark] = new_line
+            with open(self.filename, "w") as f:
+                for each_subject in subject_list:
+                    f.write(each_subject)
+            print("\t--Attendence Marked--")
 
     def view_details(self):
+        if not os.path.exists(self.filename):
+            print("Storage File does not exist,use reset option at main menu")
+            return
         subject_name = "Subject-Name"
         subject_presents = "Presents"
         subject_percentage = "Percentage"
@@ -76,53 +76,61 @@ class Bunkbuddy:
         subject_absent = "Absent"
         subject_canceled = "Canceled"
         decorator()
-        with open("bunkbuddy.txt", "r") as f:
+        with open(self.filename, "r") as f:
             subject_list = f.readlines()
             custom_decorator(117)
             print(
                 f"{subject_name:<40}{total_classs:<15}{subject_presents:<15}{subject_absent:<15}{subject_canceled:<15}{subject_percentage}")
             custom_decorator(117)
-            for i in range(len(subject_list)):
-                subject_data = subject_list[i].strip().split(
+            for i in subject_list:
+                subject_data = i.strip().split(
                     '|')
                 subject_name = subject_data[0]
                 subject_total_attend = int(subject_data[1])
                 subject_my_attend = int(subject_data[2])
                 subject_my_absents = int(subject_data[3])
                 subject_canceled = int(subject_data[4])
-                try:
+                if subject_my_attend > 0:
                     calculated_percentage = int(
                         subject_my_attend)/int(subject_total_attend)*100
                     percentage = str(calculated_percentage)+"%"
                     print(
                         f"{subject_name:<40}{subject_total_attend:<15}{subject_my_attend:<15}{subject_my_absents:<15}{subject_canceled:<15}{percentage}")
-                except ZeroDivisionError:
+                else:
                     No_data = "No Data"
                     print(
                         f"{subject_name:<40}{No_data:<15}{No_data:<15}{No_data:<15}{No_data:<15}{No_data}")
 
     def warnings(self):
-        with open("bunkbuddy.txt", "r") as f:
+        if not os.path.exists(self.filename):
+            print("Storage File does not exist,use reset option at main menu")
+            return
+        with open(self.filename, "r") as f:
 
             for subject_data in f:
                 subject_select = subject_data.strip().split('|')
                 subject_name = subject_select[0]
-                subject_percentage = int(subject_select[5])
+                subject_percentage = float(subject_select[5])
                 if subject_percentage < 75:
-                    print(f"Attendence War gi  {subject_name} di")
+                    print(
+                        f"Attendence Low for Subject :{subject_name}\nAttendence:{subject_percentage}%")
 
     def reset_data(self):
-        with open("bunkbuddy.txt", "w") as f:
+        with open(self.filename, "w") as f:
             f.write("Probability & Statistics|0|0|0|0|0\n")
             f.write("Technical & Business Writing|0|0|0|0|0\n")
             f.write("Digital Logic Design|0|0|0|0|0\n")
             f.write("Introduction to Software Engineering|0|0|0|0|0\n")
             f.write("Object Oriented Programming|0|0|0|0|0\n")
             f.write("Discrete Structures|0|0|0|0|0")
+            decorator()
             print("Reset Successfull")
 
     def bunkmeter(self):
-        with open("bunkbuddy.txt", "r") as f:
+        if not os.path.exists(self.filename):
+            print("Storage File does not exist,use reset option at main menu")
+            return
+        with open(self.filename, "r") as f:
             for subject_data in f:
                 subject_select = subject_data.strip().split('|')
                 subject_name = subject_select[0]
@@ -133,7 +141,7 @@ class Bunkbuddy:
                     while (subject_my_attend/(subject_total_attend+counter+1))*100 >= 75:
                         counter += 1
                     print(
-                        f"You can skip {counter} Classes for Subject {subject_name} ")
+                        f"Skipable classes for subject {subject_name:<55}: {counter} ")
                 except ZeroDivisionError:
                     print("No Class Data Available")
 
